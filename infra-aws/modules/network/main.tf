5372b678b569411073e78ce7487f6a1fa5f56332
TomP101 <113848215+TomP101@users.noreply.github.com>
2025-06-04 22:30:59 +0200

    fix duplicate outputs

--- infra-aws/modules/network/main.tf
+++ infra-aws/modules/network/main.tf
@@ -187,20 +187,5 @@ resource "aws_lb_listener_rule" "api_rule" {
   }
 }
 
-#
-# Outputs: DNS ALB oraz ARN obu target group
-#
-output "alb_dns_name" {
-  description = "DNS Name of the Application Load Balancer"
-  value       = aws_lb.application-lb.dns_name
-}
 
-output "frontend_tg_arn" {
-  description = "ARN of the Frontend Target Group"
-  value       = aws_lb_target_group.frontend_tg.arn
-}
 
-output "backend_tg_arn" {
-  description = "ARN of the Backend Target Group"
-  value       = aws_lb_target_group.backend_tg.arn
-}
--- infra-aws/modules/network/output.tf
+++ infra-aws/modules/network/output.tf
@@ -25,3 +25,9 @@ output "alb_listener_arn" {
   description = "ARN of the ALB listener"
   value       = aws_lb_listener.listener_forward_all.arn
 }
+
+output "alb_dns_name" {
+  description = "DNS Name of the Application Load Balancer"
+  value       = aws_lb.application-lb.dns_name
+}
+
